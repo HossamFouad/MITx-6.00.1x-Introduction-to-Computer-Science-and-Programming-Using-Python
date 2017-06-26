eb9cbc740596413fd828a3d1df8343371e7dda14
Hossam Fouad <es-hossam.fouad1318@alexu.edu.eg>
2017-06-26 03:56:30 +0200

    Hello

---
+++ Problem Set 3/Problem_1.py
@@ -0,0 +1,13 @@
+def isWordGuessed(secretWord, lettersGuessed):
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: boolean, True if all the letters of secretWord are in lettersGuessed;
+      False otherwise
+    '''
+    # FILL IN YOUR CODE HERE...
+    i=0
+    for string in secretWord:
+        if string in lettersGuessed:
+            i+=1
+    return i==len(secretWord)
